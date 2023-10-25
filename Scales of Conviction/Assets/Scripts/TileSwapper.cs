66f6d794e53ef72d602ca994e821e22a9de41870
Skatemonkey99 <simoncp29@yahoo.ca>
2023-10-25 11:42:41 -0400

    TileSwapper
    I added a tileswapper script

---
+++ Scales of Conviction/Assets/Scripts/TileSwapper.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSwapper : MonoBehaviour
+{    
+    public GameObject[] tiles;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" )
+        {
+            Destroy(gameObject);
+            CreateNewTile();
+        }
+    }
+
+    void CreateNewTile()
+    {
+        Instantiate(tiles[Random.Range(0, tiles.Length)], transform.position, transform.rotation);
+    }
+}
